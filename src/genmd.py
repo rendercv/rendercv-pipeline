f4d33b2869000a8d447a8578b003107a625bc070
Fabio Calefato <fcalefato@gmail.com>
2024-12-24 15:52:29 +0100

    Rename fixhtml.py to genmd.py and update to generate Markdown CV from HTML

--- src/genmd.py
+++ src/genmd.py
@@ -1,12 +1,21 @@
 import os
 import re
 
+preamble = """---
+layout: default
+title: Fabio Calefato Curriculum Vitae
+---
+
+"""
+
 def fix_index_html():
-    source_file = os.path.join(os.path.dirname(__file__), "..", "docs", "index.html")
+    source_file = os.path.join(os.path.dirname(__file__), "..", "docs", "Fabio_Calefato_CV.html")
+    output_file = os.path.join(os.path.dirname(__file__), "..", "docs", "index.md")
     with open(source_file, "r", encoding="utf-8") as f:
         lines = f.readlines()
 
-    with open(source_file, "w", encoding="utf-8") as f:
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write(preamble)
         for line in lines:
             # Replace ' in</h2>' at the end of the line with '  </h2>'
             fixed_line = re.sub(r" in</h2>$", "</h2>", line)
